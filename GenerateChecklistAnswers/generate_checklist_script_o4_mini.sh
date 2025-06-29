e1d67adb4cc24fb5d92e0394695b1bdc8dfe73cf
Ihsan Ullah <ihsan2131@gmail.com>
2025-06-29 23:23:05 +0500

    new sh scripts added

---
+++ GenerateChecklistAnswers/generate_checklist_script_o4_mini.sh
@@ -0,0 +1,20 @@
+
+# ICLR 2024 accepted - o4 mini
+python generate_checklistanswers_with_separate_prompts.py --input_dir data/ICLR2024_latest/accepted --output_dir checklists/o4/ICLR2024/accepted --file_pattern "./*/structured_paper_output/paper.md" --max_workers 400 --deployment_name Checklist-GPT-o4-mini --debug_timer
+# ICLR 2024 rejected - o4 mini
+python generate_checklistanswers_with_separate_prompts.py --input_dir data/ICLR2024_latest/rejected --output_dir checklists/o4/ICLR2024/rejected --file_pattern "./*/structured_paper_output/paper.md" --max_workers 400 --deployment_name Checklist-GPT-o4-mini --debug_timer
+
+# # ICML 2024 accepted - o4 mini
+# python generate_checklistanswers_with_separate_prompts.py --input_dir data/ICML2024_latest/accepted --output_dir checklists/o4/ICML2024/accepted --file_pattern "./*/structured_paper_output/paper.md" --max_workers 400 --deployment_name Checklist-GPT-o4-mini --debug_timer
+
+# # ICML 2025 accepted - o4 mini
+# python generate_checklistanswers_with_separate_prompts.py --input_dir data/ICML2025_latest/accepted --output_dir checklists/o4/ICML2025/accepted --file_pattern "./*/structured_paper_output/paper.md" --max_workers 400 --deployment_name Checklist-GPT-o4-mini --debug_timer
+# # ICML 2025 rejected - o4 mini
+# python generate_checklistanswers_with_separate_prompts.py --input_dir data/ICML2025_latest/rejected --output_dir checklists/o4/ICML2025/rejected --file_pattern "./*/structured_paper_output/paper.md" --max_workers 400 --deployment_name Checklist-GPT-o4-mini --debug_timer
+
+# # NeurIPS 2023 accepted - o4 mini
+# python generate_checklistanswers_with_separate_prompts.py --input_dir data/NeurIPS2023_latest/accepted --output_dir checklists/o4/NeurIPS2023/accepted --file_pattern "./*/structured_paper_output/paper.md" --max_workers 400 --deployment_name Checklist-GPT-o4-mini --debug_timer
+
+# # NeurIPS 2024 accepted - o4 mini
+# python generate_checklistanswers_with_separate_prompts.py --input_dir data/NeurIPS2024_latest/accepted --output_dir checklists/o4/NeurIPS2024/accepted --file_pattern "./*/structured_paper_output/paper.md" --max_workers 400 --deployment_name Checklist-GPT-o4-mini --debug_timer
+
