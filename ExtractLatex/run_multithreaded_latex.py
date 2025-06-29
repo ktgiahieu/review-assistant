928e7b00149c1296a738d5a857ae5d21e6e3b448
Khuong T.G. Hieu <ktgiahieu@gmail.com>
2025-06-29 20:05:38 +0200

    delete print

--- ExtractLatex/run_multithreaded_latex.py
+++ ExtractLatex/run_multithreaded_latex.py
@@ -364,7 +364,6 @@ def _process_single_paper_id_worker(paper_id_candidate, or_client, ar_client, de
     # print(f"[{thread_name}] Worker started for OR ID: {paper_id_candidate}")
     try:
         note = or_client.get_note(id=paper_id_candidate)
-        print(note.content)
         paper_title_original = note.content.get('title', {})
         if not isinstance(paper_title_original, str):
             paper_title_original = paper_title_original.get('value', '')
